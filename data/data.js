a492eaaf3c0ced7cc3e1f5b6f6fba95314be8f0b
DOriginalSamxie <samadrufai@gmail.com>
2023-01-23 14:45:31 +0100

    created a dashboard using Nextjs

---
+++ data/data.js
@@ -0,0 +1,113 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'Samad',
+      last: 'Rufai',
+    },
+    total: 27950.95,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'HighDee',
+      last: 'Laalaa',
+    },
+    total: 27800.95,
+    status: 'Processing',
+    method: 'PayPal',
+    date: '25 Minutes ago',
+  },
+
+  {
+    id: 3,
+    name: {
+      first: 'Muslim',
+      last: 'Tijani',
+    },
+    total: 49500.85,
+    status: 'Completed',
+    method: 'Visa',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'precious',
+      last: 'Babatunde',
+    },
+    total: 15000.45,
+    status: 'Processing',
+    method: 'MasterCard',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Steve',
+      last: 'Harding',
+    },
+    total: 1750.25,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Laura',
+      last: 'Croft',
+    },
+    total: 12950.75,
+    status: 'Completed',
+    method: 'Check',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Michael',
+      last: 'Jones',
+    },
+    total: 8900.95,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'James',
+      last: 'Bond',
+    },
+    total: 6890.45,
+    status: 'Completed',
+    method: 'Visa',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Haley',
+      last: 'Whiting',
+    },
+    total: 1400.99,
+    status: 'Completed',
+    method: 'PayPal',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Tim',
+      last: 'Thomas',
+    },
+    total: 21800.99,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '1 Day ago',
+  },
+]
